20b41062dc27d7fadc4f62a0029886d4b751bff0
Hapachino <erickwbrownfield@gmail.com>
2019-03-09 19:16:47 -0800

    ingredient model and refactored recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,9 +1,7 @@
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
-
-  constructor(name: string, description: string, imagePath: string) {
-    Object.assign(this, { name, description, imagePath });
-  }
+  constructor(
+    public name: string, 
+    public description: string, 
+    public imagePath: string
+  ) {}
 }

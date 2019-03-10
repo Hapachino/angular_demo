3fb500d4f99cacc46cc691c9f3d9f3c4887a7bf1
Hapachino <erickwbrownfield@gmail.com>
2019-03-09 18:55:03 -0800

    recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'A Test Description',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      )
+  ];
+
   constructor() { }
 
   ngOnInit() {

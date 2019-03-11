68443a62ddc0d2365c46f9b4bd2e0f4209627522
Hapachino <erickwbrownfield@gmail.com>
2019-03-10 21:31:06 -0700

    manual routing implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  recipes = false;
+  shopping = false;
+
+  renderComponent(link) {
+    switch(link.link) {
+      case 'recipes': 
+        this.shopping = false;
+        this.recipes = true;
+        break;
+      case 'shopping':
+        this.recipes = false;
+        this.shopping = true;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() linkClicked = new EventEmitter<{link: string}>();
 
+  onLinkClicked(link) {
+    this.linkClicked.emit({link});
+  }
 }

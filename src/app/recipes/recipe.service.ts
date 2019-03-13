6437c869120c0d77479d8d4a1b2b063687b517bc
Hapachino <erickwbrownfield@gmail.com>
2019-03-12 22:12:05 -0700

    added ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export default class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,20 @@ export default class RecipeService {
     new Recipe(
       'A Test Recipe',
       'A Test Description',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ],
     ),
     new Recipe(
       'Another Test Recipe',
       'Another Test Description',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ],
     )
   ];
 
